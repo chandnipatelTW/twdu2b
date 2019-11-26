71ca74da5687b334a65095cbde62464245903d4e
gmartinezramirez <gonzalo.martinez.ra@gmail.com>
2019-11-26 14:38:23 +0530

    [gmartinez/cdeguzman] Add Kafka Publish Message stage

--- e2e-test/e2e.sh
+++ e2e-test/e2e.sh
@@ -1,32 +1,28 @@
-echo "-----------------------------"
-echo "TwoWheelers E2E Test"
-echo "-----------------------------"
-
-function kafkaCreateTopic () {
-    echo "-----------------------------"
-    echo "Kafka create topic"
-    echo "-----------------------------"
-    
-}
-
-
-function kafkaDeleteTopic () {
-    echo "-----------------------------"
-    echo "Kafka delete topic"
-    echo "-----------------------------"
-
-}
-
 function kafkaPublishMessage () {
     echo "-----------------------------"
-    echo "Kafka publish message"
+    echo "1. Test: Kafka publish message"
     echo "-----------------------------"
+    kafka-console-producer --broker-list localhost:9092 --topic "station_data_test" < resources/valid_response_citybik.json
+    if [ $? -eq 0 ]
+    then
+        echo "Failure: Cant publish message in the topic in Kafka"
+        exit 1
+    else
+        echo "Sucess: publish message to Kafka"
+    fi
 
 }
 
 function waitForHDFSUpdate () {
     echo "-----------------------------"
-    echo "Wait for HDFS Update"
+    echo "2. Test: Wait for HDFS Update"
     echo "-----------------------------"
 
 }
+
+echo "-----------------------------"
+echo "TwoWheelers E2E Test"
+echo "-----------------------------"
+
+kafkaPublishMessage
+waitForHDFSUpdate
