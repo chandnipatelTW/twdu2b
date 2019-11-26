1089212c9ad49d30516400c2011ca94641ec4b4d
gmartinezramirez <gonzalo.martinez.ra@gmail.com>
2019-11-26 16:50:30 +0530

    [gmartinez/cdeguzman] Add persist_to_workspace for testing purposes

--- e2e-test/e2e.sh
+++ e2e-test/e2e.sh
@@ -4,7 +4,6 @@ function kafkaPublishMessage () {
     echo "============================="
     echo "1. Test: Kafka publish message"
     echo "============================="
-    pwd
     kafka-console-producer --broker-list localhost:9092 --topic "station_data_test" < 'resources/valid_response_citybik.json'
     if [ $? -eq 1 ]
     then
