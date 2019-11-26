5f9942831652eed571ba16183ee16162e33f3ff9
gmartinezramirez <gonzalo.martinez.ra@gmail.com>
2019-11-26 16:31:24 +0530

    [gmartinez/cdeguzman] Add pwd for debug

--- e2e-test/e2e.sh
+++ e2e-test/e2e.sh
@@ -4,8 +4,9 @@ function kafkaPublishMessage () {
     echo "============================="
     echo "1. Test: Kafka publish message"
     echo "============================="
-    kafka-console-producer --broker-list localhost:9092 --topic "station_data_test" < resources/valid_response_citybik.json
-    if [ $? -eq 0 ]
+    pwd
+    kafka-console-producer --broker-list localhost:9092 --topic "station_data_test" < 'resources/valid_response_citybik.json'
+    if [ $? -eq 1 ]
     then
         echo "Failure: Cant publish message in the topic in Kafka"
         exit 1
