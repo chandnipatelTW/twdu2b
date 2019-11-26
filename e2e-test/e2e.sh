26bb1d58ba02e5335a9ab22996c537d1ce72352d
gmartinezramirez <gonzalo.martinez.ra@gmail.com>
2019-11-26 12:18:50 +0530

    [gmartinez/cdeguzman] Add E2E script and add stage in circleci

---
+++ e2e-test/e2e.sh
@@ -0,0 +1,32 @@
+echo "-----------------------------"
+echo "TwoWheelers E2E Test"
+echo "-----------------------------"
+
+function kafkaCreateTopic () {
+    echo "-----------------------------"
+    echo "Kafka create topic"
+    echo "-----------------------------"
+    
+}
+
+
+function kafkaDeleteTopic () {
+    echo "-----------------------------"
+    echo "Kafka delete topic"
+    echo "-----------------------------"
+
+}
+
+function kafkaPublishMessage () {
+    echo "-----------------------------"
+    echo "Kafka publish message"
+    echo "-----------------------------"
+
+}
+
+function waitForHDFSUpdate () {
+    echo "-----------------------------"
+    echo "Wait for HDFS Update"
+    echo "-----------------------------"
+
+}
