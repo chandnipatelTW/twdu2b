35d26ecf0021eb618a2cb54d49d83a9a621c4c28
Pietra Thamy Sofia Da Fonseca Freitas <pietrasofia@hotmail.com>
2019-11-27 15:44:22 +0530

    [Brian/Pietra] Fix timezone issue causing test failures.

--- CitibikeApiProducer/src/test/java/com/free2wheelers/models/MessageMetadataTest.java
+++ CitibikeApiProducer/src/test/java/com/free2wheelers/models/MessageMetadataTest.java
@@ -3,23 +3,33 @@ package com.free2wheelers.models;
 import org.assertj.core.util.DateUtil;
 import org.junit.Test;
 
+import java.text.DateFormat;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
 import static org.junit.Assert.assertEquals;
 
 public class MessageMetadataTest {
 
     @Test
     public void shouldReturnStringWithMetadataAndPayload() {
-        long ingestionTime = DateUtil.parse("2017-01-01").getTime();
+        long millisecondsInUTC = LocalDateTime
+                .of(2017, 1, 1, 0, 0)
+                .atZone(ZoneId.of("UTC"))
+                .toInstant()
+                .getEpochSecond() * 1000;
+
         String producerId = "Producer-id";
         String messageUUID = "123e4567-e89b-12d3-a456-426655440000";
         long size = 12;
 
-        MessageMetadata metadata = new MessageMetadata(ingestionTime, producerId, messageUUID, size);
+        MessageMetadata metadata = new MessageMetadata(millisecondsInUTC, producerId, messageUUID, size);
 
         String expected = "{\"producer_id\": \"Producer-id\", " +
                 "\"size\": 12, " +
                 "\"message_id\": \"123e4567-e89b-12d3-a456-426655440000\", " +
-                "\"ingestion_time\": 1483246800000}";
+                "\"ingestion_time\": 1483228800000}";
 
         assertEquals(expected, metadata.toString());
     }
