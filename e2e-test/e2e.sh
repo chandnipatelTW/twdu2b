3ed41d4506afbd046680289218ff5e9e309dc685
gmartinezramirez <gonzalo.martinez.ra@gmail.com>
2019-11-27 14:45:37 +0530

    [gmartinezramirez/mairaaraujo] Add kafkaPublishMessageTest

--- e2e-test/e2e.sh
+++ e2e-test/e2e.sh
@@ -1,53 +1,72 @@
+#!/usr/bin/env bash
+
+TRAINING_GROUP=twdu2b
 WAITING_TIME_SECONDS=60
 
-JSON_DATA_TEST=$(cat <<EOF
+MESSAGE_TO_WRITE_IN_KAFKA=$(cat <<EOF
 {
-	"network": {
-		"company": [
-			"JCDecaux"
-		],
-		"href": "/v2/networks/test",
-		"id": "test",
-		"license": {
-			"name": "Open Licence",
-			"url": "https://developer.jcdecaux.com/#/opendata/licence"
-		},
-		"location": {
-			"city": "Marseille",
-			"country": "FR",
-			"latitude": 43.296482,
-			"longitude": 5.36978
-		},
-		"name": "TestStationData",
-		"source": "https://developer.jcdecaux.com",
-		"stations": [{
-			"empty_slots": 16,
-			"extra": {
-				"address": "391 MICHELET - 391 BOULEVARD MICHELET",
-				"banking": true,
-				"bonus": false,
-				"last_update": 1574754112000,
-				"slots": 19,
-				"status": "OPEN",
-				"uid": 8149
-			},
-			"free_bikes": 3,
-			"id": "686e48654a218c70daf950a4e893e5b0",
-			"latitude": 43.25402727813068,
-			"longitude": 5.401873594694653,
-			"name": "8149-391 MICHELET",
-			"timestamp": "2019-11-26T07:48:28.419000Z"
-		}]
-	}
+  "payload": {
+    "network": {
+      "company": [
+        "JCDecaux"
+      ],
+      "href": "/v2/networks/test",
+      "id": "test",
+      "license": {
+        "name": "Open Licence",
+        "url": "https://developer.jcdecaux.com/#/opendata/licence"
+      },
+      "location": {
+        "city": "Marseille",
+        "country": "FR",
+        "latitude": 43.296482,
+        "longitude": 5.36978
+      },
+      "name": "TestStationData",
+      "source": "https://developer.jcdecaux.com",
+      "stations": [
+        {
+          "empty_slots": 16,
+          "extra": {
+            "address": "391 MICHELET - 391 BOULEVARD MICHELET",
+            "banking": true,
+            "bonus": false,
+            "last_update": 1574754112000,
+            "slots": 19,
+            "status": "OPEN",
+            "uid": 8149
+          },
+          "free_bikes": 3,
+          "id": "686e48654a218c70daf950a4e893e5b0",
+          "latitude": 43.25402727813068,
+          "longitude": 5.401873594694653,
+          "name": "8149-391 MICHELET",
+          "timestamp": "2019-11-26T07:48:28.419000Z"
+        }
+      ]
+    }
+  }
 }
 EOF
 )
 
-function kafkaPublishMessage () {
+function kafkaPublishMessageTest () {
     echo "============================="
     echo "1. Test: Kafka publish message"
     echo "============================="
-    kafka-console-producer --broker-list localhost:9092 --topic "station_data_test" <<< "$JSON_DATA_TEST"
+
+	echo 'Message to write: '
+	echo "$MESSAGE_TO_WRITE_IN_KAFKA"
+		
+	# Executing in a container env
+	kafka_container=streamingdatapipeline_kafka_1
+	docker exec "$kafka_container" sh -c "echo '$MESSAGE_TO_WRITE_IN_KAFKA' | /opt/kafka_2.11-0.10.0.1/bin/kafka-console-producer.sh --broker-list localhost:9092 --topic station_data_test" 
+
+	# TODO: Executing in a remote AWS Kafka instance
+	#ssh kafka."$TRAINING_GROUP".training << EOF
+  	#	kafka-console-producer --broker-list localhost:9092 --topic "station_data_test" <<< "$JSON_DATA_TEST"
+	#EOF
+		
     if [ $? -eq 1 ]
     then
         echo "Failure: Cant publish message in the topic in Kafka"
@@ -55,14 +74,6 @@ function kafkaPublishMessage () {
     else
         echo "Sucess: publish message to Kafka"
     fi
-
-}
-
-function waitProcessToFinish () {
-    echo "-----------------------------"
-    echo "Waiting 1 minute for process to finish"
-    echo "-----------------------------"
-    sleep "$WAITING_TIME_SECONDS"
 
 }
 
