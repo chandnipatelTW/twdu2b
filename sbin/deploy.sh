44478c8811546321e8adc5e31de028d87248875a
Lorena Souza <lorena@MacBook-Pro-de-Lorena.local>
2019-11-28 14:05:08 +0530

    [Lorena] Go back to the old IP address of bastion machine

--- sbin/deploy.sh
+++ sbin/deploy.sh
@@ -17,7 +17,7 @@ Host emr-master.$TRAINING_COHORT.training
 
 Host *.$TRAINING_COHORT.training
 	ForwardAgent yes
-	ProxyCommand ssh 3.0.229.44 -W %h:%p 2>/dev/null
+	ProxyCommand ssh 52.76.28.55 -W %h:%p 2>/dev/null
 	User ec2-user
   StrictHostKeyChecking no
 " >> ~/.ssh/config
